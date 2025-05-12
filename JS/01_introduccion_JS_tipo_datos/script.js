43005e894e7094614dd7bd86c07f9bf7241fb855
martina-Lonconao <martinalonconao@liceovvh.cl>
2025-05-12 10:29:07 -0400

    ok

---
+++ JS/01_introduccion_JS_tipo_datos/script.js
@@ -0,0 +1,24 @@
+//prueba de conexion JS
+console.log("conexion establecida con JS!");
+
+//creacion de variables
+
+let num=10;   //variable INT(numero entero)
+const decimal =3.1416; //variable decimal 
+
+//creacion de funcion numeroEntero
+function numeroentero() {
+    return "variable numerica:" + num;
+}
+
+// impresion en consola 
+console.log(numeroentero());  //mostrar numero entero
+
+
+
+
+
+
+
+
+
