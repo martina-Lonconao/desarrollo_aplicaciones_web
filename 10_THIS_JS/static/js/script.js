37ad6c260b4bba367d5f6a6837b93b3040f7ab23
martina-Lonconao <martinalonconao@liceovvh.cl>
2025-09-30 09:05:21 -0300

    4.1

--- 10_THIS_JS/static/js/script.js
+++ 10_THIS_JS/static/js/script.js
@@ -1,1 +1,23 @@
+console.log("page loaded...");
 
+function liked(element){
+    const span =element.querySelector("span");
+    let like=parseInt(span.innerText);
+    like++;
+    span.innerText=like;
+}
+
+
+function login(element) {
+    if (element.innerText=="login") {
+        element.innerText="logout";
+    } else {
+        element.innerText="login";
+    }
+}
+
+
+
+function hide(el) {
+    el.remove();
+}
